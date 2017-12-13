1568ff6fc77dc3e24c8a6cfdfabed2301744fd7a
yazgoo <yazgoo@gmail.com>
2017-12-13 11:17:36 +0100

    Add listing capacity

--- plugin/setup_vmux.sh
+++ plugin/setup_vmux.sh
@@ -4,16 +4,23 @@ __is_running_in_nvim() {
 if ! __is_running_in_nvim
 then
   vmux() {
-    if [ $# -gt 1 ] && [ "$1" = attach ]
+    session_prefix="nvim-session-"
+    if [ $# -gt 0 ] && [ "$1" = list ]
+    then
+      abduco -l | grep "$session_prefix"
+    elif [ $# -gt 1 ] && [ "$1" = attach ]
     then
       abduco -e '^g' -A "$2"
-    else
+    elif [ $# -gt 0 ] && [ "$1" = new ]
+    then
       id=$(date +%s)
       server_file="/tmp/vim-server-$id"
-      session_name=nvim-session-$id
+      session_name="$session_prefix$id"
       vmux_server_file=$server_file abduco -e '^g' -A "$session_name" nvim \
         --cmd "let g:confirm_quit_nomap = 0"\
         --cmd "let g:server_addr = serverstart('$server_file')"
+    else
+      echo "please provide an action (new|attach|list)"
     fi
   }
   return
