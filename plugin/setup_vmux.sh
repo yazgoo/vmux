5a092c4b66b7451ebf2d9c4714733e06c96ed56c
yazgoo <yazgoo@gmail.com>
2018-01-31 21:51:47 +0100

    support vim

--- plugin/setup_vmux.sh
+++ plugin/setup_vmux.sh
@@ -1,4 +1,11 @@
 export PATH="$PATH:$(dirname "$0")"
+if [ $# -gt 0 ]
+then
+  VMUX_EDITOR="$1"
+else
+  VMUX_EDITOR="nvim"
+fi
+export VMUX_EDITOR
 [ -z "$vmux_server_file" ] && return
 # specific stuff to vmux session
 export EDITOR=editor
